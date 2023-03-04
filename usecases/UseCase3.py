0dfceb309497a060d2aae965fab1f293cbf31b78
Patr1ick <padi.geiger@gmail.com>
2023-03-04 15:17:05 +0100

    Added basic project structure

---
+++ usecases/UseCase3.py
@@ -0,0 +1,12 @@
+from usecases.UseCaseInterface import UseCaseInterface
+
+
+class UseCase3(UseCaseInterface):
+    def __init__(self):
+        pass
+
+    def run() -> None:
+        pass
+
+    def is_triggered() -> bool:
+        pass
