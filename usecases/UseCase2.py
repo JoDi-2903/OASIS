ec39ed1bf16e3c91d40f383649ea2ce8df38460b
alien-coding <alien-code@outlook.de>
2023-03-09 11:40:10 +0100

    Little experimenting

--- usecases/UseCase2.py
+++ usecases/UseCase2.py
@@ -1,4 +1,6 @@
 from usecases.UseCaseInterface import UseCaseInterface
+import http.client
+
 
 #Use case for midday health routine, containing:
 #   Inspirational quote (https://docs.zenquotes.io/zenquotes-documentation/#call-today)
@@ -6,10 +8,32 @@ from usecases.UseCaseInterface import UseCaseInterface
 #   health information (https://dev.fitbit.com/build/reference/web-api/developer-guide/)
 class UseCase2(UseCaseInterface):
     def __init__(self):
+        print('tewst')
         pass
 
-    def run() -> None:
+    def run(self) -> None:
+        self.get_fitness_excercise()
+
+    def is_triggered(self) -> bool:
         pass
 
-    def is_triggered() -> bool:
-        pass
+    def get_fitness_excercise(self) -> None:
+        conn = http.client.HTTPSConnection("https://api.api-ninjas.com/v1/")
+
+        headers = {
+            'X-RapidAPI-Key': "6f4dd6f642msh16d8ee2b9cb9552p118908jsn8d1f88a21ff3",
+            'X-RapidAPI-Host': "exercisedb.p.rapidapi.com"
+            }
+
+        conn.request("GET", "/exercises/exercise/1739", headers=headers)
+
+        res = conn.getresponse()
+        data = res.read()
+
+        print(data.decode("utf-8"))
+
+#Idee: https://api-ninjas.com/api/exercises
+
+
+test = UseCase2()
+test.run()
