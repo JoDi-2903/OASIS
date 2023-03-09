8e3f09c0950ce29154de5f0b9597f339b17df11d
alien-coding <alien-code@outlook.de>
2023-03-09 10:11:30 +0100

    init commit to publish branch

--- usecases/UseCase2.py
+++ usecases/UseCase2.py
@@ -1,6 +1,9 @@
 from usecases.UseCaseInterface import UseCaseInterface
 
-
+#Use case for midday health routine, containing:
+#   Inspirational quote (https://docs.zenquotes.io/zenquotes-documentation/#call-today)
+#   Yoga or fitness exercise (some exercise)
+#   health information (https://dev.fitbit.com/build/reference/web-api/developer-guide/)
 class UseCase2(UseCaseInterface):
     def __init__(self):
         pass
