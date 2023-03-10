828766f76d6322be8a03197ea8123e65b31e7c10
alien-coding <alien-code@outlook.de>
2023-03-10 15:05:33 +0100

    Made text for zen quote and yoga ex.

--- api/ZenQuotes.py
+++ api/ZenQuotes.py
@@ -1,11 +1,22 @@
+import json
+import random
+import urllib.request
 
 
 class ZenQuotes():
     def __init__(self):
         pass
 
-    def get_list_of_quotes() -> dict:
-        pass
+    def get_random_quote(self) -> dict:
+        with urllib.request.urlopen("https://zenquotes.io/api/quotes") as url:
+            list_of_quotes = json.load(url)
+            zen_quote = list_of_quotes[random.randrange(0, len(list_of_quotes))]
+        return zen_quote
 
+    # dict, where 'q' is quote and 'a' is the author
     def get_daily_quote() -> dict:
-        pass
+        with urllib.request.urlopen("https://zenquotes.io/api/today") as url:
+            quote = json.load(url)
+        return quote[0]
+
+#print(ZenQuotes().get_random_quote())  
--- usecases/UseCase2.py
+++ usecases/UseCase2.py
@@ -1,25 +1,41 @@
 from usecases.UseCaseInterface import UseCaseInterface
 from api.YogaApi import YogaApi
-
+from api.ZenQuotes import ZenQuotes
 
 #Use case for midday health routine, containing:
 #   Inspirational quote (https://docs.zenquotes.io/zenquotes-documentation/#call-today)
 #   Yoga or fitness exercise (https://yoga-api-nzy4.onrender.com/v1/poses)
 #   health information (https://dev.fitbit.com/build/reference/web-api/developer-guide/)
+
 class UseCase2(UseCaseInterface):
-    def __init__(self):
-        pass
+    def __init__(self, voice):
+        self.voice = voice
 
     def run(self) -> None:
-        self.get_fitness_exercise()
+        routine_text = "Good afternoon. This is your health routine. First, your daily Zen Quote: "
+        routine_text += self.get_zen_quote()
+        routine_text += " We'll continue with an exercise.  "
+        routine_text += self.get_fitness_exercise()
+        routine_text += " Now to your health information. Do you want to hear it?"
+        self.voice.speak(routine_text)
+        
+        print(routine_text)
 
     def is_triggered(self) -> bool:
         pass
 
-    def get_fitness_exercise(self) -> None:
+    def get_fitness_exercise(self) -> str:
         yoga_exercise = YogaApi.get_random_yoga_exercise()
+        return_string = "Your daily Yoga exercise is " + yoga_exercise['english_name'] + ". "
+        return_string += " The benefits are: " + yoga_exercise['pose_benefits'] + ". "
+        return_string += "The description: " + yoga_exercise['pose_description']
+        return return_string
 
-
+    def get_zen_quote(self) -> str:
+        zen_quote = ZenQuotes.get_daily_quote()
+        return_string = zen_quote['a'] + " once said: "
+        return_string += zen_quote['q']
+        return return_string
 
 test = UseCase2()
 test.run()
