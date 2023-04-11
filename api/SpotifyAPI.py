597a9dbc78be7acf51f33ed1b8f41848d5d1d625
aichelberg2 <aichelberg2@gmail.com>
2023-04-11 22:13:18 +0200

    Added config file for keys and customization

--- api/SpotifyAPI.py
+++ api/SpotifyAPI.py
@@ -9,29 +9,22 @@ from spotipy.exceptions import SpotifyException
 
 class SpotifyAPI():
     sp = None
-    USERNAME = 'diepoldstr'
-    CLIENT_ID = '927a5dd3a29a4c93808569a6056f3b66'
-    CLIENT_SECRET = '870601bbddb34463a186f7524f2852dd'
     REDIRECT_URI = 'http://localhost:8888/callback'
     SCOPE = 'user-modify-playback-state'
 
     def __init__(self, config):
-        self.sp = self.connectToSpotify()
         self.config = config
+        self.USERNAME = self.config.get('SPOTIFY_USERNAME')
+        self.CLIENT_ID = self.config.get('SPOTIFY_CLIENT_ID')
+        self.CLIENT_SECRET = self.config.get('SPOTIFY_CLIENT_SECRET')
+        self.playlist_id = self.config.get('playlist_id')
+        self.sp = self.connectToSpotify()
 
     def playDiningPlaylist(self):
-        playlist_id = '37i9dQZF1DXbm6HfkbMtFZ'
         playing = False
         while not playing:
             try:
-                self.sp.trace = False
-                self.sp.shuffle(state=True)
-                self.sp.start_playback(
-                    device_id=None,
-                    context_uri=f'spotify:playlist:{playlist_id}'
-                )
-                logging.info("Playing dining playlist...")
-                playing = True
+                self.startPlayback(self)
             except SpotifyException:
                 logging.info("Open Spotify on your device and try again.")
                 time.sleep(5)
@@ -46,3 +39,13 @@ class SpotifyAPI():
             # username=self.USERNAME
         )
         return spotipy.Spotify(auth_manager=auth_manager)
+
+    def startPlayback(self):
+        self.sp.trace = False
+        self.sp.shuffle(state=True)
+        self.sp.start_playback(
+            device_id=None,
+            context_uri=f'spotify:playlist:{self.playlist_id}'
+        )
+        logging.info("Playing dining playlist...")
+        playing = True
