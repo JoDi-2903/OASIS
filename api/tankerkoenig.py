431ba19f2072b0ef19e8513536428b5c09b1470e
Benjamin B <benjamin.bb@outlook.de>
2023-04-11 23:24:19 +0200

    fix bug

--- api/tankerkoenig.py
+++ api/tankerkoenig.py
@@ -4,7 +4,7 @@ import os
 class Tankerkoenig():
 
     def getGasStationData(lat, lon, gasType) -> dict:
-        url = "https://creativecommons.tankerkoenig.de/json/list.php?lat=" + str(lat)  + "&lng=" + str(lon)  +"&rad=2&sort=price&type=" + gasType +"&apikey=" + os.getenv('TANKERKOENIG_API_KEY')
+        url = "https://creativecommons.tankerkoenig.de/json/list.php?lat=" + str(lat)  + "&lng=" + str(lon)  +"&rad=2&sort=price&type=" + gasType +"&apikey=" +  str(os.getenv('TANKERKOENIG_API_KEY'))
         response = requests.get(url).json()
         lowest = None
         for station in response['stations']:
