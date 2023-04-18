69dc32119f9c5a5cad403cedfe54b678dfa523bd
BennyBB97 <101838035+BennyBB97@users.noreply.github.com>
2023-04-18 23:56:06 +0200

    fixed bug

--- pytest/usecases/test_UseCase1.py
+++ pytest/usecases/test_UseCase1.py
@@ -1,35 +1,34 @@
 from api.Tankerkoenig import Tankerkoenig
 from api.OpenWeather import OpenWeather
 from api.GoogleCalendar import GoogleCalendar
-
+import os
 # --- Tankerkoenig ---
 
 def test_with_e5_gas():
-    gasStation = Tankerkoenig.getGasStationData(48.7833056999332, 9.166720315342262, "e5")   
+    gasStation = Tankerkoenig.getGasStationData(48.7833056999332, 9.166720315342262, "e5", os.env('WEATHER_API_KEY'))   
     assert gasStation is not None
 
 def test_with_e10_gas():
-    gasStation = Tankerkoenig.getGasStationData(48.7833056999332, 9.166720315342262, "e10")   
+    gasStation = Tankerkoenig.getGasStationData(48.7833056999332, 9.166720315342262, "e10", os.env('WEATHER_API_KEY'))   
     assert gasStation is not None
 
 def test_with_diesel_gas():
-    gasStation = Tankerkoenig.getGasStationData(48.7833056999332, 9.166720315342262, "diesel")   
+    gasStation = Tankerkoenig.getGasStationData(48.7833056999332, 9.166720315342262, "diesel", os.env('WEATHER_API_KEY'))   
     assert gasStation is not None
 
 def test_all_station_fields_filled():
-    gasStation = Tankerkoenig.getGasStationData(48.7833056999332, 9.166720315342262, "e5") 
+    gasStation = Tankerkoenig.getGasStationData(48.7833056999332, 9.166720315342262, "e5", os.env('WEATHER_API_KEY')) 
     assert gasStation['brand'] != ''
     assert gasStation['price'] > 0
     assert gasStation['street'] != ''
-    assert gasStation['houseNumber'] != ''
     assert gasStation['place'] != ''
 
 # --- OpenWeather ---
 def test_getWeatherData():
-    assert OpenWeather.getWeatherData(48.7833056999332, 9.166720315342262) is not None
+    assert OpenWeather.getWeatherData(48.7833056999332, 9.166720315342262, os.env('WEATHER_API_KEY')) is not None
 
 def test_WeatherData():
-    weatherInfo, currentTemp, minTemp, maxTemp =  OpenWeather.getWeatherData(48.7833056999332, 9.166720315342262)
+    weatherInfo, currentTemp, minTemp, maxTemp =  OpenWeather.getWeatherData(48.7833056999332, 9.166720315342262, os.env('WEATHER_API_KEY'))
     assert weatherInfo != ''
     assert minTemp < 100 and minTemp > -50
     assert currentTemp < 100 and currentTemp > -50
