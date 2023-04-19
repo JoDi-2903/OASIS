428f5724c79817e6d564d98dd4bd8351e9928fa1
BennyBB97 <101838035+BennyBB97@users.noreply.github.com>
2023-04-19 13:00:47 +0200

    delete credential.json and changed to config.json.. fix Calender issue

--- api/GoogleCalendar.py
+++ api/GoogleCalendar.py
@@ -24,7 +24,7 @@ class GoogleCalendar():
                 creds.refresh(Request())
             else:
                 flow = InstalledAppFlow.from_client_secrets_file(
-                    'credentials.json', SCOPES)
+                    'config.json', SCOPES)
                 creds = flow.run_local_server(port=0)
             # Save the credentials for the next run
             with open('token.json', 'w') as token:
